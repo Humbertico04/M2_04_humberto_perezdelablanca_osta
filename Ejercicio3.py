7d173f9845ac115db093e3dc4387125aeb5879df
Humbertico04 <113980694+Humbertico04@users.noreply.github.com>
2022-10-07 10:08:56 +0000

    Ej.4

--- Ejercicio3.py
+++
@@ -1,9 +0,0 @@
-A=float(input("Dime un número: "))
-B=float(input("Dime otro número: "))
-C=float(input("Dime otro número más: "))
-
-print("\n")
-
-lista=[A, B, C]
-sumatorio=sum(lista)
-print("Esos números suman:", sumatorio)
